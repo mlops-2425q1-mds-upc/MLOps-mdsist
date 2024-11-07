be4fea6c8d078134ac9e4e156e7a8d55e4c2882f
Zhengyong8119 <jizyong@gmail.com>
2024-11-07 23:55:35 +0100

    Update api.py

--- mdsist/api.py
+++ mdsist/api.py
@@ -6,14 +6,15 @@ import uvicorn
 
 app = FastAPI()
 
-## Saps on tenim el nostre model entrenat?
+## Saps on tenim el nostre model entrenat? we can import it with pickel or joblib
 model = joblib.dump("our_trained_mnist_model")
 
 class PredictionRequest(BaseModel):
-    image: list
+    image: list # The input data should be an array list of 256 pixel
 
 @app.post("/mnist-model-prediction/")
 async def predict(data: PredictionRequest):
     image_array = np.array(data.image).reshape(1,-1)
     prediction = model.predict(image_array)
     return{"prediction": int(prediction[0])}
+
