291f7f578bf9a056a1f925825ed38b6ff1baa475
Ignasi Cervero <ignasicervero99@gmail.com>
2024-12-11 19:57:44 +0100

    Fix evidently script.
    Avoid crashing  if csv does not exist. Set evidently keep updating metrics

--- mdsist/evidently_app.py
+++ mdsist/evidently_app.py
@@ -3,7 +3,7 @@ Evidently script for computing metrics and exposing them so prometheus can colle
 """
 
 import time
-
+import os
 import pandas as pd
 from evidently.metric_preset import ClassificationPreset, DataDriftPreset
 from evidently.pipeline.column_mapping import ColumnMapping
@@ -18,41 +18,49 @@ pl_drift_gauge = Gauge("predicted_label_drift_score", "Predicted Label Drift Met
 cur_accuracy_gauge = Gauge("current_model_accuracy", "Current Model Accuracy")
 ref_accuracy_gauge = Gauge("reference_model_accuracy", "Reference Model Accuracy")
 
-# Load your data
-reference_data = pd.read_csv(f"{MONITORING_DIR}/reference_data.csv")[
-    ["true_label", "predicted_label"]
-]
-current_data = pd.read_csv(f"{MONITORING_DIR}/current_data.csv")[["true_label", "predicted_label"]]
-
-# Map columns (adjust for your dataset)
-column_mapping = ColumnMapping(target="true_label", prediction="predicted_label")
-
-# Set up Evidently report with specific metrics
-data_drift_report = Report(metrics=[DataDriftPreset()])
-classification_report = Report(metrics=[ClassificationPreset()])
-
-# Generate reports
-data_drift_report.run(
-    reference_data=reference_data, current_data=current_data, column_mapping=column_mapping
-)
-classification_report.run(
-    reference_data=reference_data, current_data=current_data, column_mapping=column_mapping
-)
-
-# Metrics extraction
-tl_drift_score = data_drift_report.as_dict()["metrics"][1]["result"]["drift_by_columns"][
-    "true_label"
-]["drift_score"]
-pl_drift_score = data_drift_report.as_dict()["metrics"][1]["result"]["drift_by_columns"][
-    "predicted_label"
-]["drift_score"]
-cur_accuracy = classification_report.as_dict()["metrics"][0]["result"]["current"]["accuracy"]
-ref_accuracy = classification_report.as_dict()["metrics"][0]["result"]["reference"]["accuracy"]
-
-
 # Export metrics to Prometheus
 start_http_server(8100)  # Prometheus scraper will pull from this port
 while True:
+
+    reference_data_path = f"{MONITORING_DIR}/reference_data.csv"
+    current_data_path = f"{MONITORING_DIR}/current_data.csv"
+
+    # Check that there is data to report
+    if not os.path.exists(reference_data_path) or not os.path.exists(current_data_path):
+        time.sleep(60)
+        continue
+
+    # Load your data
+    reference_data = pd.read_csv(reference_data_path)[
+        ["true_label", "predicted_label"]
+    ]
+    current_data = pd.read_csv(current_data_path)[["true_label", "predicted_label"]]
+
+    # Map columns (adjust for your dataset)
+    column_mapping = ColumnMapping(target="true_label", prediction="predicted_label")
+
+    # Set up Evidently report with specific metrics
+    data_drift_report = Report(metrics=[DataDriftPreset()])
+    classification_report = Report(metrics=[ClassificationPreset()])
+
+    # Generate reports
+    data_drift_report.run(
+        reference_data=reference_data, current_data=current_data, column_mapping=column_mapping
+    )
+    classification_report.run(
+        reference_data=reference_data, current_data=current_data, column_mapping=column_mapping
+    )
+
+    # Metrics extraction
+    tl_drift_score = data_drift_report.as_dict()["metrics"][1]["result"]["drift_by_columns"][
+        "true_label"
+    ]["drift_score"]
+    pl_drift_score = data_drift_report.as_dict()["metrics"][1]["result"]["drift_by_columns"][
+        "predicted_label"
+    ]["drift_score"]
+    cur_accuracy = classification_report.as_dict()["metrics"][0]["result"]["current"]["accuracy"]
+    ref_accuracy = classification_report.as_dict()["metrics"][0]["result"]["reference"]["accuracy"]
+
     print("updating metrics...")
     tl_drift_gauge.set(tl_drift_score)
     pl_drift_gauge.set(pl_drift_score)
